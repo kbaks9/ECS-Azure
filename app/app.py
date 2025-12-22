421655eda225763f4bfcf3555b91e87e13f2dd1c
kbaks9 <kamaal.bakar@outlook.com>
2025-12-22 21:07:52 +0000

    Changed port to 8080.

--- app/app.py
+++ app/app.py
@@ -56,4 +56,4 @@ def delete_task(task_id):
     return jsonify({'result': True})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
