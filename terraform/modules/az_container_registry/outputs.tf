435154acf8734e719793b0b61caec0e6c8b5e0c7
kbaks9 <kamaal.bakar@outlook.com>
2025-12-30 19:38:58 +0000

    More architectural implementation

--- terraform/modules/az_container_registry/outputs.tf
+++ terraform/modules/az_container_registry/outputs.tf
@@ -1,7 +1,1 @@
-output "container_registry_id" {
-    value = azurerm_container_registry.acr.id
-}
 
-output "container_registry_login_server" {
-    value = azurerm_container_registry.acr.login_server
-}
