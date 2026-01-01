280d83b1978cbb887b7135c24db16335f9149dbd
kbaks9 <kamaal.bakar@outlook.com>
2026-01-01 18:04:25 +0000

    Creating container registry

--- terraform/modules/az_container_registry/main.tf
+++ terraform/modules/az_container_registry/main.tf
@@ -1,31 +1,9 @@
-#resource "azurerm_container_registry" "acr" {
-#  name                = var.acr_name
-#  resource_group_name = var.resource_group_name
-#  location            = var.location
-#  sku                 = "Basic"
-#  admin_enabled       = false
-#}
-
-/* # Taken from Terraform documentation site
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+# Creating Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = var.name_acr
+  resource_group_name = var.resource_group
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = false
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Premium"
-  admin_enabled       = false
-  georeplications {
-    location                = "East US"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-}*/
