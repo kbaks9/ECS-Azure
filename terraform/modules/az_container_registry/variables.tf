2b0626644d32000ddf8a6567cb3acf8df422a155
kbaks9 <kamaal.bakar@outlook.com>
2026-01-01 18:05:30 +0000

    Adding variables for registry module

---
+++ terraform/modules/az_container_registry/variables.tf
@@ -0,0 +1,20 @@
+# Resource Group
+variable "resource_group" {
+  type        = string
+  description = "Name of the resource group"
+  default     = "rg-taskmanager"
+}
+
+# Location
+variable "location" {
+  type        = string
+  description = "value"
+  default     = "uksouth"
+}
+
+# ACR 
+variable "name_acr" {
+  type        = string
+  description = "Name of the ACR"
+  default     = "taskmanagerapp"
+}
