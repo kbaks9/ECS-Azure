eee3c84482f01f2f7a6acec4bc84b6b9e7cb0689
kbaks9 <kamaal.bakar@outlook.com>
2026-01-09 02:20:00 +0000

    Updating modularisation and applying best practice

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,19 +17,37 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
 }
 
-# 
-# module "az_container_app" {
-#   source = "./modules/az_container_app"
-# }
+module "az_container_registry" {
+  source         = "./modules/az_container_registry"
+  name           = "cntaskmanager"
+  resource_group = var.resource_group
+  location       = var.location
+}
+
+module "az_container_app" {
+  source         = "./modules/az_container_app"
+  app_name       = "taskmanageracr"
+  env_name       = "tm-env"
+  resource_group = var.resource_group
+  location       = var.location
+  container_name = "tm-app"
+  image          = "${module.az_container_registry.acr_login_server}/tm-app:latest"
+  cpu            = 0.25
+  memory         = "0.5Gi"
+}
+
+module "role_assignment" {
+  source       = "./modules/role_assignment"
+  principal_id = module.az_container_app.principal_id
+  role_name    = "AcrPull"
+  scope        = module.az_container_registry.acr_id
+}
 
 # module "network" {
 #   source = "./modules/network"
 # }
 
-module "az_container_registry" {
-  source         = "./modules/az_container_registry"
-  resource_group = var.resource_group
-  location       = var.location
-}
+# module "front_door" {
+# source = "./modules/front_door"
+# }
 
-## Need to create more modules
