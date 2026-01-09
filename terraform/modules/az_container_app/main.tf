129b852ac508ef3e710705c5408b3c6f12c573b1
kbaks9 <kamaal.bakar@outlook.com>
2026-01-09 02:25:16 +0000

    Changing hardcoded values to variables

--- terraform/modules/az_container_app/main.tf
+++ terraform/modules/az_container_app/main.tf
@@ -1,1 +1,34 @@
+resource "azurerm_container_app_environment" "app_env" {
+  name                = var.env_name
+  location            = var.location
+  resource_group_name = var.resource_group
+}
 
+resource "azurerm_container_app" "app" {
+  name                         = var.app_name
+  container_app_environment_id = azurerm_container_app_environment.app_env.id
+  resource_group_name          = var.resource_group
+  revision_mode                = "Single"
+
+  identity {
+    type = "SystemAssigned" # <-- System Assigned identity
+  }
+
+  template {
+    container {
+      name   = var.container_name
+      image  = var.image
+      cpu    = var.cpu
+      memory = var.memory
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 8080
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+}
