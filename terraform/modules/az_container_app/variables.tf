4da1ca505aee060ba60f4298842debbf9da117e3
kbaks9 <kamaal.bakar@outlook.com>
2026-01-09 02:26:39 +0000

    Emptied the variables so they can be called within root module

--- terraform/modules/az_container_app/variables.tf
+++ terraform/modules/az_container_app/variables.tf
@@ -1,38 +1,8 @@
-#### General Settings
-
-# Resource Group
-variable "resource_group" {
-  type        = string
-  description = "Name of the resource group"
-  default     = "rg-taskmanager"
-}
-
-# Location
-variable "location" {
-  type        = string
-  description = "value"
-  default     = "uksouth"
-}
-
-###Container Application
-
-# Azure Container Application
-variable "app_name" {
-  type        = string
-  description = "Azure Container Application"
-  default     = "taskmanager"
-}
-
-# Azure Container Environment
-variable "cn_env" {
-  type        = string
-  description = "Azure Container Environment"
-  default     = "tm-env"
-}
-
-# Container name within the Application
-variable "cn_name" {
-  type        = string
-  description = "value"
-  default     = "tm-app"
-}
+variable "app_name" { type = string }
+variable "env_name" { type = string }
+variable "resource_group" { type = string }
+variable "location" { type = string }
+variable "container_name" { type = string }
+variable "image" { type = string }
+variable "cpu" { type = number }
+variable "memory" { type = string }
