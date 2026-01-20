4f0049a1fae7e5251b3b6ab95b5075ad186f841b
kbaks9 <kamaal.bakar@outlook.com>
2026-01-20 21:09:42 +0000

    Removing default values from .tf file and also updating variables

--- terraform/modules/az_container_app/variables.tf
+++ terraform/modules/az_container_app/variables.tf
@@ -1,12 +1,48 @@
-variable "app_name" { type = string }
-variable "env_name" { type = string }
-variable "resource_group" { type = string }
-variable "location" { type = string }
-variable "container_name" { type = string }
-variable "image" { type = string }
-variable "cpu" { type = number }
-variable "memory" { type = string }
-variable "acr_login_server" { type = string }
+variable "app_name" {
+  description = "Name of the Azure Container App"
+  type        = string
+}
+
+variable "env_name" {
+  description = "Name of the Azure Container App Environment"
+  type        = string
+}
+
+variable "resource_group" {
+  description = "Name of the resource group where resources will be deployed"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+}
+
+variable "container_name" {
+  description = "Name of the container inside the Container App"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Name of the image tag"
+  type        = string
+}
+
+# variable "cpu" {
+#   description = "CPU cores allocated to the container"
+#   type        = number
+# }
+
+# variable "memory" {
+#   description = "Memory allocated to the container (e.g. 0.5Gi)"
+#   type        = string
+# }
+
+variable "acr_login_server" {
+  description = "ACR login server URL"
+  type        = string
+}
+
 variable "acr_id" {
   description = "The full resource ID of the Azure Container Registry"
   type        = string
