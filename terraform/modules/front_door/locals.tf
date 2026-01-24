ff8cb999debcd49173d6253a7923e430bd80b016
kbaks9 <kamaal.bakar@outlook.com>
2026-01-24 14:45:15 +0000

    Locally intialised variables

---
+++ terraform/modules/front_door/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  profile_name   = "fd-profile"
+  sku_name       = "Standard_AzureFrontDoor"
+  ep_name        = "ep-name"
+  fd_group_name  = "group-name"
+  fd_origin_name = "origin-name"
+  fd_route_name  = "route-name"
+}
