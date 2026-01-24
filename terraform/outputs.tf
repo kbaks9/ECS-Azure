0495811729dbdbfd3f17ec0dce2db11ee1609a62
kbaks9 <kamaal.bakar@outlook.com>
2026-01-24 14:50:06 +0000

    Fully commented out variables.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "acr_login_server" {
   value = module.az_container_registry.acr_login_server
 }
+
+output "fqdn" {
+  value = module.az_container_app.fqdn
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,47 @@
-### General
+# # ### General
 
-# Resource Group
-variable "resource_group" {
-  type        = string
-  description = "Name of the resource group"
-  default     = "rg-taskmanager"
-}
+# # Resource Group
+# variable "resource_group" {
+#   type        = string
+#   description = "Name of the resource group"
+#   default     = "rg-taskmanager"
+# }
 
-# Location
-variable "location" {
-  type        = string
-  description = "value"
-  default     = "uksouth"
-}
+# # Location
+# variable "location" {
+#   type        = string
+#   description = "value"
+#   default     = "uksouth"
+# }
 
-### Networking
-## To be added.
+# ###
+
+# # Container
+
+# variable "app_name" {
+#   description = "Name of the Azure Container App"
+#   type        = string
+#   default     = "taskmanageracr"
+# }
+
+# variable "env_name" {
+#   description = "Name of the Azure Container App Environment"
+#   type        = string
+#   default     = "tm-env"
+# }
+
+# variable "container_name" {
+#   description = "Name of the container inside the Container App"
+#   type        = string
+#   default     = "tm-app"
+# }
+
+# variable "image_tag" {
+#   description = "Name of the image tag"
+#   type        = string
+#   default     = "latest"
+# }
+
+
+# # ### Networking
+# # ## To be added.
