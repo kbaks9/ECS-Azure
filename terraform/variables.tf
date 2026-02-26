680daf23b12ef15c24657f106d948c73323d64a5
kbaks9 <kamaal.bakar@outlook.com>
2026-02-26 04:32:12 +0000

    Added network variables for module:network

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -74,3 +74,25 @@ variable "fd_route_name" {
   type        = string
   description = "The front door routing name"
 }
+
+### Network ###
+variable "virtual_network_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "virtual_network_address_space" {
+  description = "Address space for the virtual network"
+  type        = string
+}
+
+variable "container_app_subnet_name" {
+  description = "Name of the subnet for container apps"
+  type        = string
+}
+
+variable "container_app_subnet_prefix" {
+  description = "Address prefix for the subnet"
+  type        = string
+}
+
