57ccccf6554287aa299cf9aef9b869038bf27015
kbaks9 <kamaal.bakar@outlook.com>
2026-02-26 04:47:55 +0000

    Remove locals.tf from repo

--- terraform/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  resource_group = "rg-taskmanager"
-  location       = "uksouth"
-  app_name       = "taskmanageracr"
-  env_name       = "tm-env"
-  container_name = "tm-app"
-  image_tag      = "latest"
-}
